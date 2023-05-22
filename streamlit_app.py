15175c82a2561a3e8e36d629cfc24d3f337be386
wyounane <133870806+wyounane@users.noreply.github.com>
2023-05-22 14:48:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
+my_cur = my_cnx.cursor()
 #my_cur.execute("USE ROLE <SYSADMIN>")
 #my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 #my_data_row = my_cur.fetchone()
