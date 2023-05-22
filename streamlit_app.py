dd078598dc9be85e054d7ec7213110d9b7f9fc80
wyounane <133870806+wyounane@users.noreply.github.com>
2023-05-22 14:47:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#use role pc_rivery_role
-#use warehouse pc_rivery_wh
-my_cur.execute("USE ROLE <SYSADMIN>")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("USE ROLE <SYSADMIN>")
+#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+#my_data_row = my_cur.fetchone()
+#streamlit.text("The fruit load list contains:")
+#streamlit.text(my_data_row)
