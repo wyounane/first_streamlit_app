bb0aaa6f82736439d53d04824c26f9e9d9a440da
wyounane <133870806+wyounane@users.noreply.github.com>
2023-05-23 10:56:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,15 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to addt?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+
+# take the json version of the response and normalize it
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it the screen as a table
+#streamlit.dataframe(fruityvice_normalized)
